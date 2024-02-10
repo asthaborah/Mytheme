9f30a45e4c872a66d8b0178d8c0dab8b5fe05617
asthaborah <89447681+asthaborah@users.noreply.github.com>
2024-02-10 15:47:01 +0530

    changed file name and added new screenshot image

--- wp-content/themes/MyCustomTheme/functions.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    //this function is used to bring menu option in the admin appearance section
-    register_nav_menus(
-        //array is created to add different types of menus for example , header menu , sidebar menu
-        array('primary-menu' => 'Header Menu')
-    )
-?>
-

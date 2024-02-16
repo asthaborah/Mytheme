f6240f582a1b3d57d7f3cb2ff4e03e839abb4dca
asthaborah <89447681+asthaborah@users.noreply.github.com>
2024-02-16 15:13:02 +0530

    added custom logo and navbar

--- wp-content/themes/cornus/header.php
+++ wp-content/themes/cornus/header.php
@@ -1,24 +1,33 @@
-<?php 
+<?php
 /*
-*header
-*@package cornus
-*/
+ *header
+ *@package cornus
+ */
 
 ?>
 
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>">
+
 <head>
-    <meta charset="<?php bloginfo('charset')?>">
+    <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cornus</title>
+    <!-- <title>Cornus</title> -->
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-    <?php 
-        if(function_exists('wp_body_open()')){
-            wp_body_open();
-        }
+    <?php
+    if (function_exists('wp_body_open()')) {
+        wp_body_open();
+    }
     ?>
-    <header>This is header</header>
-    
+
+    <div id="page" class="site">
+        <header id='masthead' class='site-header' role='banner'>
+            <?php get_template_part('template-parts/header/nav'); ?>
+
+
+        </header>
+
+    <div id="content" class="site-content">
--- wp-content/themes/cornus/inc/classes/class-cornus-theme.php
+++ wp-content/themes/cornus/inc/classes/class-cornus-theme.php
@@ -26,10 +26,18 @@ class CORNUS_THEME{
          * Actions.
          */
 
-        // add_action('after-')
+        add_action('after_setup_theme' , [$this , 'setup_theme']);
     }
 
     public function setup_theme(){
-
+        //dynamically add site title in the wordpress theme
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo' , [
+            'header-text' => ['site-title' , 'site-description'], 
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true
+        ]);
     }
 }

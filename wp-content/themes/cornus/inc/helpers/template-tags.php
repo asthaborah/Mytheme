a832ce7ce4e1121abd29969ea78c31867ca4e80b
asthaborah <89447681+asthaborah@users.noreply.github.com>
2024-02-23 01:54:12 +0530

    Added date time to all the posts

--- wp-content/themes/cornus/inc/helpers/template-tags.php
+++ wp-content/themes/cornus/inc/helpers/template-tags.php
@@ -38,3 +38,28 @@ function the_post_custom_thumbnail($post_id , $size = 'featured-thumbnail' , $ad
     echo get_the_post_custom_thumbnail($post_id , $size , $additional_attributes);
 }
 
+
+function Cornus_posted_on(){
+    $time_string = '<time class = "entry-date published updated" datetime = "%1$s"> %2$s </time>';
+
+    //when post is modified
+    if( get_the_time('U') !== get_the_modified_time('U')){
+        $time_string = '<time class = "entry-date published" datetime = "%1$s"> %2$s</time><time class = "updated" datetime = "%3$s"> %4$s </time>';
+    }
+
+    $time_string = sprintf( $time_string , 
+    esc_attr( get_the_date(DATE_W3C)) ,
+    esc_attr( get_the_date()) ,
+    esc_attr( get_the_modified_date(DATE_W3C)) ,
+    esc_attr( get_the_modified_date()) ,
+    );
+
+    $posted_on = sprintf(
+        esc_html_x('Posted on %s' , 'post date' , 'Cornus'),
+        '<a href = "'  .esc_url(get_permalink()) .  '" rel = "bookmark">' . $time_string . '</a>'
+    );
+
+    echo '<span class = "posted-on text-secondary">'. $posted_on .'</span>';
+
+}
+
---
+++ wp-content/themes/cornus/template-parts/components/blog/entry-meta.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Template for entry meta
+ * 
+ */
+?>
+
+<div class="entry-meta mb-3" style="text-align: center;">
+ <?php Cornus_posted_on() ?>
+</div>

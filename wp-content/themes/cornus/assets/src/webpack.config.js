f5152790131f48a9ce9f1170ad772391dbb66a6d
asthaborah <asthaborah086@gmail.com>
2024-03-06 22:02:23 +0530

    configured webpack config file

--- wp-content/themes/cornus/assets/src/webpack.config.js
+++ wp-content/themes/cornus/assets/src/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require( 'path' );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+
 
 const JS_DIR = path.resolve(__dirname , '/src/js');
 const IMG_DIR = path.resolve(__dirname , '/src/img');
@@ -13,8 +16,58 @@ const output = {
     filename : 'js/[name].js'
 };
 
+const rules = [
+    {
+        // Rules for javascript file
+        test: /\.js$/,
+        include: [ JS_DIR ],
+        exclude: /node_modules/,
+        use: 'babel-loader'
+    },
+
+    {
+        // Rules for css file
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader , 'css-loader'],
+    },
+
+    {
+        // Rules for images and files
+        test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
+        use:[
+                {
+                    loader: 'file-loader',
+                    Options: {
+                        name: '[path][name].[ext]',
+                        publicPath: 'production' === process.env.NODE_ENV ? '../' : '../..',
+                    },
+                },
+            ],
+    }
+];
+
+const plugins = ( argv ) => {
+    //plugin for cleaning unused assets and output files on rebuild
+    new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: ( 'production' === argv.mode)
+    }),
+
+    //plugin for extracting css after bundling of files
+    new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+    })
+};
+
 module.exports = (env , argv) => ({
     entry : entry,
     output : output,
-    devtool : 'source-map'
+    devtool : 'source-map',
+    module : {
+        rules : rules,
+    },
+    plugins: plugins( argv ),
+    externals: {
+        jquery: 'jQuery'
+    }
 });

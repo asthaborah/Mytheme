c685e0c9dbf9d2ff5746efd5cd031beb9dd3c54a
asthaborah <asthaborah086@gmail.com>
2024-03-12 20:59:54 +0530

    created template parts for block content

--- wp-content/themes/cornus/inc/classes/class-block-patterns.php
+++ wp-content/themes/cornus/inc/classes/class-block-patterns.php
@@ -28,36 +28,54 @@ class Block_Patterns{
     public function register_block_patterns(){
         // since this is a new feature and for backward compatibility we are checking this
         if(function_exists('register_block_pattern')){
+            /**
+             * Cover Pattern
+             */
+            $cover_content = $this->get_pattern_content('template-parts/patterns/cover');
+
             register_block_pattern(
                 'Cornus/cover',
                 [
                     'title' => __( 'Cornus Cover' , 'Cornus' ),
                     'description' => __('Cornus Cover Block with image and text' , 'Cornus'),
                     'categories' => ['cover'],
-                    'content' => '<!-- wp:cover {"url":"http://localhost:8080/Mytheme/wp-content/uploads/2024/03/singapore-1990959_1280.jpg","id":135,"dimRatio":50,"focalPoint":{"x":0.66,"y":0.43},"align":"full","layout":{"type":"constrained"}} -->
-                    <div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-135" alt="" src="http://localhost:8080/Mytheme/wp-content/uploads/2024/03/singapore-1990959_1280.jpg" style="object-position:66% 43%" data-object-fit="cover" data-object-position="66% 43%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"large"} -->
-                    <h1 class="wp-block-heading has-text-align-center has-large-font-size"><strong>Never let your memories be greater than your dreams</strong></h1>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:heading {"textAlign":"center","level":4,"fontSize":"medium"} -->
-                    <h4 class="wp-block-heading has-text-align-center has-medium-font-size">A mind that is stretched by a new experience can never go back to its old dimensions.</h4>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-                    <div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"gradient":"linear-gradient(135deg,rgb(20,8,8) 0%,rgb(155,81,224) 100%)"}},"className":"is-style-outline"} -->
-                    <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-background wp-element-button" style="background:linear-gradient(135deg,rgb(20,8,8) 0%,rgb(155,81,224) 100%)">Blogs</a></div>
-                    <!-- /wp:button --></div>
-                    <!-- /wp:buttons --></div></div>
-                    <!-- /wp:cover -->'
+                    'content' => $cover_content,
+                ]
+            );
+
+            /**
+             * Two Column Pattern
+             */
+            $cover_content = $this->get_pattern_content('template-parts/patterns/two-columns');
+
+            register_block_pattern(
+                'Cornus/two-columns',
+                [
+                    'title' => __( 'Cornus Two Columns' , 'Cornus' ),
+                    'description' => __('Cornus two columns Block with heading and text' , 'Cornus'),
+                    'categories' => ['two-columns'],
+                    'content' => $cover_content,
                 ]
             );
         }
     }
 
+    public function get_pattern_content($template_path){
+        ob_start();
+
+        get_template_part($template_path);
+
+        $pattern_content = ob_get_contents();
+
+        ob_end_clean();
+
+        return $pattern_content;
+    }
+
     public function register_block_patterns_categories(){
         $pattern_categories = [
             'cover' => __('Cover' , 'Cornus'),
-            'carousel' => __('Caraosel' , 'Cornus'),
+            'two-columns' => __('Two-columns' , 'Cornus'),
         ];
 
         if(!empty($pattern_categories) && is_array($pattern_categories)){
---
+++ wp-content/themes/cornus/template-parts/patterns/two-columns.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * two columns block pattern template
+ */
+?>
+<!-- wp:columns {"align":"wide","backgroundColor":"black","className":"cornus-two-column-pattern"} -->
+<div class="wp-block-columns alignwide cornus-two-column-pattern has-black-background-color has-background"><!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|luminous-vivid-amber"}}}},"textColor":"luminous-vivid-amber"} -->
+        <h2 class="wp-block-heading has-luminous-vivid-amber-color has-text-color has-link-color">Heading</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+        <p class="has-white-color has-text-color has-link-color"><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column -->
+    <div class="wp-block-column"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|luminous-vivid-amber"}}}},"textColor":"luminous-vivid-amber"} -->
+        <h2 class="wp-block-heading has-luminous-vivid-amber-color has-text-color has-link-color">Heading</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+        <p class="has-white-color has-text-color has-link-color"><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->

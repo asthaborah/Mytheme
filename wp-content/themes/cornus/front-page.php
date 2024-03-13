e5d9707826a6d79545d3ea3c24d519d607ec4c71
asthaborah <asthaborah086@gmail.com>
2024-03-13 23:22:45 +0530

    Added css for gutenberg

--- wp-content/themes/cornus/front-page.php
+++ wp-content/themes/cornus/front-page.php
@@ -1,12 +1,35 @@
-<?php 
+<?php
+
 /**
  * Front page template
  * 
  * @package Cornus
  */
 
- get_header();
+get_header();
 ?>
 
-<div>Front Page</div>
-<?php get_footer(); ?>
+<div id="primary" style="text-align: center;">
+    <main id="main" class="site-main mt-5" role="main">
+        <div class="home-page-wrap">
+            <?php
+            if (have_posts()) :  // checks if the post is there or not 
+                // loop starts here 
+                while (have_posts()) : the_post();
+
+                    get_template_part('template-parts/content' , 'page');
+
+                endwhile;
+                ?>
+                <?php
+            else :
+                get_template_part('template-parts/content-none');
+            endif;
+                ?>
+        </div>
+    </main>
+</div>
+
+<?php
+get_footer();
+?>
